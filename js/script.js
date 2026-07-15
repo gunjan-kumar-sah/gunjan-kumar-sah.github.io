9d6fce3dae350257a0eaec5edc2cd5df60280465
Gunjan kumar sah <gunjankrsah802128@gmail.com>
2026-07-16 00:49:38 +0530

    Add typing effect for developer roles display
    Implement typing effect for a list of developer roles.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,65 @@
-console.log("Website Loaded");
+const words=[
+
+"Software Developer",
+
+"Python Developer",
+
+"AI Enthusiast",
+
+"Web Developer"
+
+];
+
+let i=0;
+
+let j=0;
+
+let current="";
+
+let isDeleting=false;
+
+function type(){
+
+current=words[i];
+
+if(isDeleting){
+
+document.getElementById("typing").textContent=current.substring(0,j--);
+
+}
+
+else{
+
+document.getElementById("typing").textContent=current.substring(0,j++);
+
+}
+
+if(!isDeleting && j===current.length){
+
+isDeleting=true;
+
+setTimeout(type,1000);
+
+return;
+
+}
+
+if(isDeleting && j===0){
+
+isDeleting=false;
+
+i++;
+
+if(i===words.length){
+
+i=0;
+
+}
+
+}
+
+setTimeout(type,isDeleting?60:120);
+
+}
+
+type();
